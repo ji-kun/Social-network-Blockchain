22e4bd215ebd837092d7f25ac6c632fad0bab3c9
Rohit Kunji <srinivasan.kunji@gmail.com>
2020-09-15 03:08:19 +0530

    Prefinal Night Changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,11 @@ import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 
+import SocialNetwork from '../abis/SocialNetwork.json';
+import Navbar from './Navbar';
+import Main from './Main';
+import Identicon from 'identicon.js';
+
 class App extends Component {
 
   async componentWillMount() {
@@ -29,60 +34,78 @@ class App extends Component {
     const accounts = await web3.eth.getAccounts()
     console.log(accounts)
     this.setState({ account: accounts[0] })
+    //NetworkId
+    const networkId = await web3.eth.net.getId()
+    const networkData = SocialNetwork.networks[networkId]
+    if( networkData ) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      this.setState({ socialNetwork })
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({ postCount })
+      //Load Posts
+      for(var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts: [...this.state.posts, post]
+        })
+      }
+      this.setState({
+        posts: this.state.posts.sort((a,b) => b.tipAmount - a.tipAmount )
+      })
+      this.setState({loading: false})
+    }
+    else {
+      window.alert('Not Deployed')
+    }
+    //Address
+    //ABI
+  }
+
+  createPost(content) {
+    this.setState({loading: true})
+    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.account })
+    .once('reciept', (reciept) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  tipPost(id, tipAmount) {
+    this.setState({loading: true})
+    this.state.socialNetwork.methods.tipPost(id).send({ from: this.state.account, value: tipAmount })
+    .once('reciept', (reciept) => {
+      this.setState({ loading: false })
+    })
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      account:''
+      account:'',
+      socialNetwork: null,
+      postCount: 0,
+      posts: [],
+      loading: true
     }
+
+    this.createPost = this.createPost.bind(this)
+    this.tipPost = this.tipPost.bind(this)
   }
 
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-secondary">
-              <small id = "account">{this.state.account}</small>
-            </small>
-          </li>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
+        <Navbar account={this.state.account} />
+        {this.state.loading ?
+          <div id="loader" className="text-center mt-5">
+            <p>Loading ...</p>
           </div>
-        </div>
+          :
+          <Main 
+            posts ={this.state.posts}
+            createPost = {this.createPost}
+            tipPost = {this.tipPost}
+          />
+        }
       </div>
     );
   }
