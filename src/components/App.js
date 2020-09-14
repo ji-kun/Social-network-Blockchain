31d4edbf70e5a01f7bf692a74beb9089bdc2835d
Rohit Kunji <srinivasan.kunji@gmail.com>
2020-09-14 15:39:30 +0530

    Link Frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if( window.ethereum ) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if( window.web3 ) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. Try Metamask!')
+    }
+  } 
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    this.setState({ account: accounts[0] })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account:''
+    }
+  }
+
   render() {
     return (
       <div>
@@ -15,6 +50,11 @@ class App extends Component {
           >
             Dapp University
           </a>
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id = "account">{this.state.account}</small>
+            </small>
+          </li>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
